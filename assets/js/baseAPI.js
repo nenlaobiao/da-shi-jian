95ceed43fb8a5040beb071eba382aa52a2eebc00
Ming <c2083117697@qq.com>
2022-05-21 10:25:08 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,21 @@
 $.ajaxPrefilter((option) => {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = `http://www.liulongbin.top:3007` + option.url;
+    if(option.url.indexOf('/my/')){
+        option.headers={
+         Authorization:localStorage.getItem('token')
+        }
+    }
+    option.complete=(res)=>{
+        console.log(1);
+        if(
+            res.responseJSON.status ===1 &&
+            res.responseJSON.message ==="身份认证失败！"
+        ){
+            localStorage.removeItem('token');
+            location.href='/login.html'
+            console.log(2);
+        }
+    }
+    
   });
--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,9 @@ function getUserInfo(){
     $.ajax({
         type: 'GET',
         url:'/my/userinfo',
-        headers:{
-            Authorization:localStorage.getItem('token')
-        }, 
+        // headers:{
+        //     Authorization:localStorage.getItem('token')
+        // }, 
         success:(res)=>{
             console.log(res);
             if(res.status!==0){
@@ -18,7 +18,19 @@ function getUserInfo(){
             }
             layer.msg('获取用户信息成功！')
             randerAvatar(res.data)
-        }
+        }, 
+        // complete:(res)=>{
+        //     console.log(1);
+        //     if(
+        //         res.responseJSON.status ===1 &&
+        //         res.responseJSON.message ==="身份认证失败！"
+        //     ){
+        //         localStorage.removeItem('token');
+        //         location.href='/login.html'
+        //         console.log(2);
+        //     }
+        // }
+        
     })
 }
 
@@ -35,3 +47,16 @@ const randerAvatar =(user)=>{
         $('.text-avatar').html(firstName).show()
     }
 }
+ // 退出登录
+$("#btnLogout").click(() => {
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+});
